fe211714712ee06d8c8f37bc1c680a30e546914b
Nadzeya <lbonanno225@gmail.com>
2023-02-24 12:49:50 +0100

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const numberAbs = Math.abs(n);
+    const numberToString = String(numberAbs);
+    const arrayOfNumbers = numberToString.split("");
+    const arrayReverse = arrayOfNumbers.reverse();
+    const gluedNumbers = +arrayReverse.join("");
+    return gluedNumbers;
 }
